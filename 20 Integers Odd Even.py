e24a342810e90f6b588a4690fe81698a7acb4348
Joshua Lemuel Z. Centina <joshualemuelcentina@gmail.com>
2023-04-22 19:31:39 +0800

    pseudocode

--- 20 Integers Odd Even.py
+++ 20 Integers Odd Even.py
@@ -1,2 +1,13 @@
 # Joshua Lemuel Z. Centina BSCPE 1-4
 # 20 Integers Odd Even Text Files
+
+# Write a Python program that reads a text file named numbers.txt that contains 20 integers. 
+# The program will create two other text file; the first text file will be named even.txt that will contains all even numbers extracted 
+# from the numbers.txt. The second text file will be named odd.txt that will contains all odd numbers extracted from the numbers.txt.
+
+# open numbers.txt (read), even.txt (append), odd.txt (append)
+# read integers line by line
+# if even
+#   write to even.txt
+# else:
+#   write to odd.txt    
