30d674c75270b392019ff06be818ee9e29fc0dee
Joshua Lemuel Z. Centina <joshualemuelcentina@gmail.com>
2023-04-23 16:21:12 +0800

    def process, created numbers.txt, printed numbers.txt for testing

--- 20 Integers Odd Even.py
+++ 20 Integers Odd Even.py
@@ -5,9 +5,14 @@
 # The program will create two other text file; the first text file will be named even.txt that will contains all even numbers extracted 
 # from the numbers.txt. The second text file will be named odd.txt that will contains all odd numbers extracted from the numbers.txt.
 
+def process():
 # open numbers.txt (read), even.txt (append), odd.txt (append)
-# read integers line by line
-# if even
-#   write to even.txt
-# else:
-#   write to odd.txt    
+    with open("numbers.txt") as input_numbers, open("even.txt", "a") as output_even, open("odd.txt", "a") as output_odd:
+        # read integers line by line
+        for line in input_numbers:
+            print(line.strip())
+    # if even
+    #   write to even.txt
+    # else:
+    #   write to odd.txt
+process()
